a06cf292a68eb4bd93caae0942436f16d6ea5732
Pablo <pabloh202rdp@gmail.com>
2022-05-12 20:32:31 -0300

    Projeto Alura

---
+++ java script.js
@@ -0,0 +1,56 @@
+const inputTexto= document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
+inputTexto.focus();
+
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value);
+    mensagem.value = textoEncriptado;
+    mensagem.style.backgroundImage="none";
+
+
+}
+
+function btnDesencriptar() {
+    const textoDesencriptar = desencriptar(inputTexto.value);
+    mensagem.value = textoDesencriptar;
+    mensagem.style.backgroundImage="none";
+}
+
+ function btnCopiar(){
+     let copiarTexto = document.querySelector(".mensagem");
+     copiarTexto.select();
+     document.execCommand("cut");
+    inputTexto.value = "";
+
+    
+}
+
+
+
+function encriptar(textoParaEncriptar) { 
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "orber"], ["u", "ufat"]  ]
+    textoParaEncriptar = textoParaEncriptar.toLowerCase();
+
+
+ for(let i=0; i < matrizCodigo.length; i++ ) {
+        if(textoParaEncriptar.includes(matrizCodigo[i][0])) {
+            textoParaEncriptar = textoParaEncriptar.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1] )
+        }
+    }
+    return textoParaEncriptar;
+}
+
+function desencriptar(textoDesencriptar) { 
+    let matrizCodigo = [["enter", "e"], ["imes", "i"], ["ai", "a"],["orber", "o"], ["ufat", "u"] ] 
+    textoDesencriptar = textoDesencriptar.toLowerCase();
+
+   for(let i=0; i < matrizCodigo.length; i++ ) {
+      if(textoDesencriptar.includes(matrizCodigo[i][0])){
+          textoDesencriptar = textoDesencriptar.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+      }
+   }
+return textoDesencriptar;
+}
+
+    
